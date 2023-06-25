b31de45ab16ebe3c1d7660f0ede1cd6743fdb20e
dario-ramos <dario.fenix@gmail.com>
2023-06-25 20:07:28 -0300

    Format mjl_heap code

--- mjl_heap.c
+++ mjl_heap.c
@@ -31,7 +31,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-  "$Id: mjl_heap.c,v 1.11 2012/04/27 05:20:42 mjl Exp $";
+    "$Id: mjl_heap.c,v 1.11 2012/04/27 05:20:42 mjl Exp $";
 #endif
 
 #include <stdlib.h>
@@ -47,16 +47,16 @@ static const char rcsid[] =
 
 struct heap_node
 {
-  int   id;
+  int id;
   void *item;
 };
 
 struct heap
 {
-  heap_node_t   **a;
-  int             N;
-  int             max;
-  heap_cmp_t      cmp;
+  heap_node_t **a;
+  int N;
+  int max;
+  heap_cmp_t cmp;
   heap_onremove_t onremove;
 };
 
@@ -80,17 +80,17 @@ static void heap_assert(const heap_t *heap)
 #define heap_assert(heap)((void)0)
 #endif
 
-static void upheap(heap_t *heap, int k)
+static void upheap (heap_t *heap, int k)
 {
   heap_node_t *v = heap->a[k];
 
-  while(k > 1 && heap->cmp(heap->a[k/2]->item, v->item) <= 0)
-    {
-      heap->a[k] = heap->a[k/2];
-      heap->a[k]->id = k;
+  while (k > 1 && heap->cmp (heap->a[k / 2]->item, v->item) <= 0)
+  {
+    heap->a[k] = heap->a[k / 2];
+    heap->a[k]->id = k;
 
-      k = k/2;
-    }
+    k = k / 2;
+  }
 
   heap->a[k] = v;
   v->id = k;
@@ -98,26 +98,26 @@ static void upheap(heap_t *heap, int k)
   return;
 }
 
-static void downheap(heap_t *heap, int k)
+static void downheap (heap_t *heap, int k)
 {
   heap_node_t *v = heap->a[k];
   int j;
 
-  while(k <= heap->N/2)
-    {
-      j = k+k;
+  while (k <= heap->N / 2)
+  {
+    j = k + k;
 
-      if(j < heap->N && heap->cmp(heap->a[j]->item, heap->a[j+1]->item) < 0)
-	j++;
+    if (j < heap->N && heap->cmp (heap->a[j]->item, heap->a[j + 1]->item) < 0)
+      j++;
 
-      if(heap->cmp(v->item, heap->a[j]->item) >= 0)
-	break;
+    if (heap->cmp (v->item, heap->a[j]->item) >= 0)
+      break;
 
-      heap->a[k] = heap->a[j];
-      heap->a[k]->id = k;
+    heap->a[k] = heap->a[j];
+    heap->a[k]->id = k;
 
-      k = j;
-    }
+    k = j;
+  }
 
   heap->a[k] = v;
   heap->a[k]->id = k;
@@ -126,7 +126,7 @@ static void downheap(heap_t *heap, int k)
 }
 
 #ifndef DMALLOC
-heap_t *heap_alloc(heap_cmp_t cmp)
+heap_t* heap_alloc (heap_cmp_t cmp)
 #else
 heap_t *heap_alloc_dm(heap_cmp_t cmp, const char *file, const int line)
 #endif
@@ -134,48 +134,48 @@ heap_t *heap_alloc_dm(heap_cmp_t cmp, const char *file, const int line)
   heap_t *heap = NULL;
 
 #ifndef DMALLOC
-  heap = malloc(sizeof(heap_t));
+  heap = malloc (sizeof(heap_t));
 #else
   heap = dmalloc_malloc(file, line, sizeof(heap_t), DMALLOC_FUNC_MALLOC, 0, 0);
 #endif
 
-  if(heap == NULL)
+  if (heap == NULL)
     goto err;
 
-  heap->N        = 0;
-  heap->max      = HEAP_GROWBY;
-  heap->cmp      = cmp;
+  heap->N = 0;
+  heap->max = HEAP_GROWBY;
+  heap->cmp = cmp;
   heap->onremove = NULL;
 
-  if((heap->a = malloc(sizeof(heap_node_t *) * heap->max)) == NULL)
+  if ((heap->a = malloc (sizeof(heap_node_t*) * heap->max)) == NULL)
     goto err;
 
   return heap;
 
- err:
-  heap_free(heap, NULL);
+err:
+  heap_free (heap, NULL);
   return NULL;
 }
 
-void heap_free(heap_t *heap, heap_free_t free_func)
+void heap_free (heap_t *heap, heap_free_t free_func)
 {
   int i;
 
-  if(heap == NULL)
+  if (heap == NULL)
     return;
 
-  if(heap->a != NULL)
+  if (heap->a != NULL)
+  {
+    for (i = 1; i <= heap->N; i++)
     {
-      for(i=1; i <= heap->N; i++)
-	{
-	  if(free_func != NULL)
-	    free_func(heap->a[i]->item);
-	  free(heap->a[i]);
-	}
-
-      free(heap->a);
+      if (free_func != NULL)
+        free_func (heap->a[i]->item);
+      free (heap->a[i]);
     }
-  free(heap);
+
+    free (heap->a);
+  }
+  free (heap);
   return;
 }
 
@@ -184,24 +184,24 @@ void heap_free(heap_t *heap, heap_free_t free_func)
  *
  * items in the heap might violate the heap condition.  remake the heap.
  */
-void heap_remake(heap_t *heap)
+void heap_remake (heap_t *heap)
 {
   int i;
 
-  for(i=1; i<=heap->N; i++)
-    upheap(heap, i);
+  for (i = 1; i <= heap->N; i++)
+    upheap (heap, i);
 
   return;
 }
 
-void heap_onremove(heap_t *heap, heap_onremove_t onremove)
+void heap_onremove (heap_t *heap, heap_onremove_t onremove)
 {
   heap->onremove = onremove;
   return;
 }
 
 #ifndef DMALLOC
-heap_node_t *heap_insert(heap_t *heap, void *ptr)
+heap_node_t* heap_insert (heap_t *heap, void *ptr)
 #else
 heap_node_t *heap_insert_dm(heap_t *heap, void *ptr, const char *file,
 			    const int line)
@@ -216,39 +216,40 @@ heap_node_t *heap_insert_dm(heap_t *heap, void *ptr, const char *file,
 
   /* allocate a new node */
 #ifndef DMALLOC
-  node = malloc(size);
+  node = malloc (size);
 #else
   node = dmalloc_malloc(file, line, size, DMALLOC_FUNC_MALLOC, 0, 0);
 #endif
 
-  if(node == NULL)
+  if (node == NULL)
     goto err;
 
-  node->id   = heap->N+1;
+  node->id = heap->N + 1;
   node->item = ptr;
 
   /* determine if we need to increase the size of the array for this node */
-  if(node->id >= heap->max)
-    {
-      size = (heap->max + HEAP_GROWBY) * sizeof(heap_node_t *);
-      if((tmp = realloc(heap->a, size)) == NULL)
-	goto err;
+  if (node->id >= heap->max)
+  {
+    size = (heap->max + HEAP_GROWBY) * sizeof(heap_node_t*);
+    if ((tmp = realloc (heap->a, size)) == NULL)
+      goto err;
 
-      heap->max += HEAP_GROWBY;
-      heap->a = (heap_node_t **)tmp;
-    }
+    heap->max += HEAP_GROWBY;
+    heap->a = (heap_node_t**) tmp;
+  }
 
   /* insert the new node, and then satisfy the heap condition */
   heap->a[node->id] = node;
   heap->N++;
-  upheap(heap, heap->N);
+  upheap (heap, heap->N);
 
   heap_assert(heap);
 
   return node;
 
- err:
-  if(node != NULL) free(node);
+err:
+  if (node != NULL)
+    free (node);
   return NULL;
 }
 
@@ -257,10 +258,10 @@ heap_node_t *heap_insert_dm(heap_t *heap, void *ptr, const char *file,
  *
  * return the node at the top of the heap, without removing it.
  */
-heap_node_t *heap_head_node(heap_t *heap)
+heap_node_t* heap_head_node (heap_t *heap)
 {
   heap_assert(heap);
-  if(heap->N == 0)
+  if (heap->N == 0)
     return NULL;
   return heap->a[1];
 }
@@ -270,22 +271,22 @@ heap_node_t *heap_head_node(heap_t *heap)
  *
  * return the item at the top of the heap, without removing it.
  */
-void *heap_head_item(heap_t *heap)
+void* heap_head_item (heap_t *heap)
 {
   heap_assert(heap);
-  if(heap->N == 0)
+  if (heap->N == 0)
     return NULL;
   return heap->a[1]->item;
 }
 
-void *heap_remove(heap_t *heap)
+void* heap_remove (heap_t *heap)
 {
   heap_node_t *v;
   void *item;
 
   heap_assert(heap);
 
-  if(heap->N == 0)
+  if (heap->N == 0)
     return NULL;
 
   v = heap->a[1];
@@ -293,40 +294,40 @@ void *heap_remove(heap_t *heap)
   heap->a[1] = heap->a[heap->N--];
   heap->a[1]->id = 1;
 
-  downheap(heap, 1);
+  downheap (heap, 1);
 
   item = v->item;
-  free(v);
+  free (v);
 
   heap_assert(heap);
 
-  if(heap->onremove != NULL)
-    heap->onremove(item);
+  if (heap->onremove != NULL)
+    heap->onremove (item);
 
   return item;
 }
 
-void heap_foreach(heap_t *heap, void *param, heap_foreach_t func)
+void heap_foreach (heap_t *heap, void *param, heap_foreach_t func)
 {
   int i;
 
-  for(i=1; i <= heap->N; i++)
-    func(param, heap->a[i]->item);
+  for (i = 1; i <= heap->N; i++)
+    func (param, heap->a[i]->item);
 
   return;
 }
 
-int heap_count(heap_t *heap)
+int heap_count (heap_t *heap)
 {
   return heap->N;
 }
 
-void *heap_node_item(heap_node_t *node)
+void* heap_node_item (heap_node_t *node)
 {
   return node->item;
 }
 
-int heap_node_id(heap_node_t *node)
+int heap_node_id (heap_node_t *node)
 {
   return node->id;
 }
@@ -337,7 +338,7 @@ int heap_node_id(heap_node_t *node)
  * take the last node in the heap and replace the node to be deleted with
  * it.  then, satisfy the heap condition.
  */
-void heap_delete(heap_t *heap, heap_node_t *node)
+void heap_delete (heap_t *heap, heap_node_t *node)
 {
   heap_node_t *v;
   int i;
@@ -348,35 +349,35 @@ void heap_delete(heap_t *heap, heap_node_t *node)
   assert(node->id > 0);
   assert(heap->a[node->id] == node);
 
-  if(node->id == heap->N)
-    {
-      heap->N--;
-    }
+  if (node->id == heap->N)
+  {
+    heap->N--;
+  }
   else
+  {
+    /*
+     * take the last node and put it in the array where the value being
+     * deleted is
+     */
+    heap->a[node->id] = v = heap->a[heap->N--];
+    heap->a[node->id]->id = node->id;
+
+    /* if the priority of the item being replaced is raised, upheap */
+    if ((i = heap->cmp (v->item, node->item)) > 0)
     {
-      /*
-       * take the last node and put it in the array where the value being
-       * deleted is
-       */
-      heap->a[node->id] = v = heap->a[heap->N--];
-      heap->a[node->id]->id = node->id;
-
-      /* if the priority of the item being replaced is raised, upheap */
-      if((i = heap->cmp(v->item, node->item)) > 0)
-	{
-	  upheap(heap, node->id);
-	}
-      /* if the priority of the item being replaced is lowered, downheap */
-      else if(i < 0)
-	{
-	  downheap(heap, node->id);
-	}
+      upheap (heap, node->id);
     }
+    /* if the priority of the item being replaced is lowered, downheap */
+    else if (i < 0)
+    {
+      downheap (heap, node->id);
+    }
+  }
 
-  if(heap->onremove != NULL)
-    heap->onremove(node->item);
+  if (heap->onremove != NULL)
+    heap->onremove (node->item);
 
-  free(node);
+  free (node);
 
   heap_assert(heap);
 
